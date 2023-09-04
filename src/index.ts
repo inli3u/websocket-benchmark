ffb3df7c0c300b1fec150b65f845825fc67fd4c9
Alex Dunn <alex@logicallydunn.com>
2023-09-04 13:11:20 -0400

    Basic websocket server and CLI

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,29 @@
-// Put a CLI here
-//
+import { program, InvalidArgumentError } from 'commander';
+import { listen } from './server';
+
+const DEFAULT_PORT = 8080;
+
+function validateInt(value: string) {
+  const parsed = parseInt(value, 10);
+  if (isNaN(parsed)) {
+    throw new InvalidArgumentError('Not a valid int');
+  }
+  return parsed;
+}
+
+program
+  .option('--port [number]', `Defaults to ${DEFAULT_PORT}`, validateInt);
+
+program.parse();
+
+const opts = program.opts();
+const port = opts.port ?? DEFAULT_PORT;
+
+listen(port);
+console.log('Listening on port', port);
+
 // output:
 // - running on...
 // - { connections: n, event: connect | disconnect | socket-error | server-error | message }
 // 
 // eventually only output aggregate stats
-
-console.log('hey');
--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,19 @@
-// Put a basic server here
-// - ping
-// - error handling
+import { WebSocketServer } from 'ws';
+
+export function listen(port: number) {
+  const wss = new WebSocketServer({ port });
+
+  wss.on('connection', (ws) => {
+    ws.on('error', (e) => {
+      console.error('Socket error', e);
+    });
+
+    ws.on('message', (data) => {
+      console.log('Socket message', data);
+    });
+  });
+
+  wss.on('error', (e) => {
+    console.error('Server error', e);
+  });
+}
